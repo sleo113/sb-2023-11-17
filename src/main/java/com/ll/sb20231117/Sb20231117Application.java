16273a5583e8e5ec80f8d9596aa92e7f23c694c9
JeUk Park <qkrwpdnr4531@naver.com>
2023-11-17 09:47:23 +0900

    setting

---
+++ src/main/java/com/ll/sb20231117/Sb20231117Application.java
@@ -0,0 +1,13 @@
+package com.ll.sb20231117;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb20231117Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb20231117Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sb20231117/Sb20231117ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sb20231117;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb20231117ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
